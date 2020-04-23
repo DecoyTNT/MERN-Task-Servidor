2e743795d17781bd71a0cfae52055a5741a0dc3a
DecoyTNT <42523466+DecoyTNT@users.noreply.github.com>
2020-04-23 01:32:09 -0500

    Subiendo a heroku

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(cors());
 // Habilitar express.json, este se usa en vez de bodyparser
 app.use(express.json({ extended: true }));
 
-const port = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use(require('./routes/index'));
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+    console.log(`El servidor esta corriendo en el puerto ${port}`);
 });
